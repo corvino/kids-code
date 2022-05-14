b74f41e93f71d4af2c1e8cd705da909c4a40e5e0
Nathan Corvino <nathan@corvino.com>
2022-05-14 16:19:28 -0700

    Add turtle graphics lesson and speaker graphics.

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ import "highlight.js/styles/vs2015.css";
 import Reveal from "reveal.js";
 import Highlight from "reveal.js/plugin/highlight/highlight";
 import Markdown from "reveal.js/plugin/markdown/markdown";
+import Notes from "reveal.js/plugin/notes/notes";
 
 Reveal.initialize({
   hash: true,
-  plugins: [ Markdown, Highlight ]
+  plugins: [ Highlight, Markdown, Notes ]
 });
